164e52798629c81f622d036e67e0796039bf5674
Stefano Vidaurre Olite <svidaurre@domingoalonsogroup.com>
2023-03-28 12:26:36 +0200

    chore: move Tiles to file

--- TicTacToe.Application/Game.cs
+++ TicTacToe.Application/Game.cs
@@ -1,35 +1,5 @@
 ﻿namespace TicTacToe.Application;
 
-public class Tiles
-{
-    private readonly IDictionary<Tile, Player> _dictionary;
-    
-    public Tiles()
-    {
-        _dictionary = new Dictionary<Tile, Player>();
-    }
-    
-    public void AddPlay(Player player, Tile tile)
-    {
-        if (_dictionary.ContainsKey(tile))
-        {
-            throw new InvalidOperationException("The tile is busy");
-        }
-
-        _dictionary.Add(tile, player);
-    }
-
-    public Player GetTileValue(Tile tile)
-    {
-        if (_dictionary.ContainsKey(tile))
-        {
-            return _dictionary[tile];
-        }
-
-        return Player.None;
-    }
-}
-
 public class Game
 {
     private Player _lastPlay;
---
+++ TicTacToe.Application/Tiles.cs
@@ -0,0 +1,31 @@
+﻿namespace TicTacToe.Application;
+
+public class Tiles
+{
+    private readonly IDictionary<Tile, Player> _dictionary;
+    
+    public Tiles()
+    {
+        _dictionary = new Dictionary<Tile, Player>();
+    }
+    
+    public void AddPlay(Player player, Tile tile)
+    {
+        if (_dictionary.ContainsKey(tile))
+        {
+            throw new InvalidOperationException("The tile is busy");
+        }
+
+        _dictionary.Add(tile, player);
+    }
+
+    public Player GetTileValue(Tile tile)
+    {
+        if (_dictionary.ContainsKey(tile))
+        {
+            return _dictionary[tile];
+        }
+
+        return Player.None;
+    }
+}
